7a0ffd8d2aa2395acb34110f7770c879325abce0
dag0035 <dag0035@alu.ubu.es>
2017-03-07 20:55:13 +0100

    Commit inicial

---
+++ src/main/java/es/ubu/lsi/common/MessageType.java
@@ -0,0 +1,16 @@
+package es.ubu.lsi.common;
+
+/**
+ * Message type.
+ * 
+ * @author Raúl Marticorena
+ * @author Joaquin P. Seco
+ */
+public enum MessageType {
+	/** Message. */
+	MESSAGE,
+	/** Shutdown server. */
+	SHUTDOWN,		
+	/** Logout client. */
+	LOGOUT;		
+}
