cb6ea3a5c3addadd9c8ea2b0f651a664d14278e2
dag0035 <dag0035@alu.ubu.es>
2017-03-13 15:53:08 +0100

    Esqueleto

--- src/main/java/es/ubu/lsi/client/ChatClientImpl.java
+++ src/main/java/es/ubu/lsi/client/ChatClientImpl.java
@@ -1,7 +1,40 @@
 package es.ubu.lsi.client;
 
+import es.ubu.lsi.common.ChatMessage;
+
 public class ChatClientImpl implements ChatClient {
 
+	public String server;
+	public String username;
+	public int port;
+	public boolean carryOn = true;
+	public int id;
+	
+	public ChatClientImpl (String server, int port, String username){
+		this.server = server;
+		this.port = port;
+		this.username = username;
+	}
+	
+	public boolean start(){
+		//TODO 
+		return true;
+	}
+	
+	public void sendMessage( ChatMessage msg){
+		//TODO
+	}
+	
+	public void desconnect(){
+		//TODO
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	
 }
 
 class ChatClientListener implements Runnable {
--- src/main/java/es/ubu/lsi/server/ChatServerImpl.java
+++ src/main/java/es/ubu/lsi/server/ChatServerImpl.java
@@ -1,7 +1,41 @@
 package es.ubu.lsi.server;
 
+import java.text.SimpleDateFormat;
+
+import es.ubu.lsi.common.ChatMessage;
+
 public class ChatServerImpl implements ChatServer{
 
+	public final int DEFAULT_PORT = 1500;
+	public int clientId;
+	public SimpleDateFormat sdf;
+	public int port;
+	public boolean alive;
+	
+	public ChatServerImpl(int port){
+		this.port = port;
+	}
+	
+	public void stratup(){
+		
+	}
+	
+	public void shutdown(){
+		
+	}
+	
+	public void broadcast(ChatMessage message){
+		
+	}
+	
+	public void remove(int id){
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
 }
 
 class ServerThreadForClient implements Runnable{
